d822dd330201d126720f891a5a207faae2fb2da7
catenoid <hackphilippa@gmail.com>
2016-04-25 10:13:43 +0100

    Player sphere reflects both ways

--- arrow_key_movement.cs
+++ arrow_key_movement.cs
@@ -11,33 +11,52 @@ public class arrow_key_movement : MonoBehaviour {
     Vector3 convexMoveLeft = new Vector3(1,0,-1);
     Vector3 concaveMoveUp = new Vector3(-0.2357023f, -0.9428091f, -0.2357023f); // I have failed the Linear Algebra gods
     Vector3 concaveMoveLeft = new Vector3(-1,0,1);
-	
-	Matrix4x4 reflection = new Matrix4x4 ();
+    
+    Vector3 convexMoveDown;
+    Vector3 concaveMoveDown;
+    
+    Matrix4x4 reflection = new Matrix4x4 ();
+    
+    //Rigidbody rb;
     
     void Start() {
         cavexFlag = true;
         moveUp = convexMoveUp;
         moveLeft = convexMoveLeft;
         
-		reflection.SetRow (0, new Vector4(0.333f, -0.666f, -0.666f, 0.666f));
-		reflection.SetRow (1, new Vector4(-0.666f, 0.333f, -0.666f, 0.666f));
-		reflection.SetRow (2, new Vector4(-0.666f, -0.666f, 0.333f, 0.666f));
-		reflection.SetRow (3, new Vector4(0, 0, 0, 1));
+        convexMoveDown = Vector3.Cross(convexMoveUp, convexMoveLeft).normalized;
+        concaveMoveDown = Vector3.Cross(concaveMoveUp, concaveMoveLeft).normalized;
+        
+        reflection.SetRow (0, new Vector4(0.333f, -0.666f, -0.666f, 0.666f));
+        reflection.SetRow (1, new Vector4(-0.666f, 0.333f, -0.666f, 0.666f));
+        reflection.SetRow (2, new Vector4(-0.666f, -0.666f, 0.333f, 0.666f));
+        reflection.SetRow (3, new Vector4(0, 0, 0, 1));
+        
+        //rb = GetComponent<Rigidbody>();
     }
     
-	void Update () {
+    void OnCollisionEnter(Collision collision) {
+        Debug.Log("Collision PLAYER SPHERE");
+    }
+    
+    void Update () {
         if (Input.GetKeyDown(KeyCode.Space)) {
-            transform.position = reflection.MultiplyPoint3x4(transform.position);
-            
+            // Freeze the ball before inversion
+            // Also need to change the direction of gravity
+            //rb.velocity = Vector3.zero;
             if (cavexFlag) {
+                // On inversion, the player object appears to become it's reflection, were the floor a mirror.
+                transform.position += convexMoveDown;
                 moveUp = concaveMoveUp;
                 moveLeft = concaveMoveLeft;
                 cavexFlag = false;
             } else {
+                transform.position += concaveMoveDown;
                 moveUp = convexMoveUp;
                 moveLeft = convexMoveLeft;
                 cavexFlag = true;
             }
+            transform.position = reflection.MultiplyPoint3x4(transform.position);
         }
         
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
@@ -69,5 +88,5 @@ public class arrow_key_movement : MonoBehaviour {
         }
         
         transform.position += 0.05F*velocity;
-	}
+    }
 }
