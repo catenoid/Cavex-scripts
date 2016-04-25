a0b7082606c3b4317efe0aa3b50ea3bdf16d0f90
catenoid <hackphilippa@gmail.com>
2016-04-25 11:15:49 +0100

    Camera rotates instead of inverting abruptly

--- Camera_flip.cs
+++ Camera_flip.cs
@@ -4,17 +4,52 @@ using System.Collections;
 public class Camera_flip : MonoBehaviour {
     bool cavexFlag = true;
     Quaternion targetView;
+    bool rotating = false;
+    bool clockwise = true;
+    float z = 0;
+    float nextZ;
+    float rotationSpeed = 60;
     
     void Update() {
         if (Input.GetKeyDown(KeyCode.Space)) {
-            if (cavexFlag) {
-                targetView = Quaternion.Euler(35.26f,225f,180f);
-                cavexFlag = false;
+            if (rotating && clockwise) {
+                clockwise = false;
             } else {
-                targetView = Quaternion.Euler(35.26f,225f,0f);
-                cavexFlag = true;
+                clockwise = true;
+                rotating = true;
             }
-            transform.rotation = targetView;
+        }
+        if (rotating) {
+            if (clockwise) {
+                nextZ = z + Time.deltaTime * rotationSpeed;
+                if (z >= 0 && z < 180) {
+                    if (nextZ >= 180) {
+                        rotating = false;
+                        nextZ = 180;
+                    }
+                } else if (z >= 180) {
+                    if (nextZ >= 360) {
+                        rotating = false;
+                        nextZ = 0;
+                    }
+                }
+            } else {
+                // counter-clockwise rotation
+                nextZ = z - Time.deltaTime * rotationSpeed;
+                if (z >= 0 && z < 180) {
+                    if (nextZ < 0) {
+                        rotating = false;
+                        nextZ = 0;
+                    }
+                } else if (z >= 180) {
+                    if (nextZ < 180) {
+                        rotating = false;
+                        nextZ = 180;
+                    }
+                }
+            }
+            transform.rotation = Quaternion.Euler(35.26f,225f,nextZ);
+            z = nextZ;
         }
     }
 }
