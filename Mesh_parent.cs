bfbcbc6386a19ada6522317c5a3141475746ed87
catenoid <hackphilippa@gmail.com>
2016-04-27 20:35:58 +0100

    Removed defunct alternate indices calculation

--- Mesh_parent.cs
+++ Mesh_parent.cs
@@ -6,17 +6,12 @@ using System.Collections;
 abstract public class Mesh_parent : MonoBehaviour {
     protected Vector3[] concaveVerts; //needed
     Vector3[] convexVerts;
-    
-    int[] concaveIndices;
-    int[] convexIndices;
-    
+    int[] indices;
     bool cavexFlag = true;
     
     void Start () {
         convexVerts = new Vector3[concaveVerts.Length];
-        
-        concaveIndices = new int[concaveVerts.Length];
-        convexIndices = new int[convexVerts.Length];
+        indices = new int[concaveVerts.Length];
         
         // Perform reflection
         for (int i=0; i<concaveVerts.Length; i++) {
@@ -25,20 +20,14 @@ abstract public class Mesh_parent : MonoBehaviour {
         
         // The vertices are added in triples corresponding to a mesh triangle. Generate the sequence 0 1 2 3 4 5 ...
         for (int i=0; i<concaveVerts.Length; i++) {
-            concaveIndices[i] = i;
-        }
-
-        // The surface normals invert. Generate the sequence 0 2 1 3 5 4 6 8 7 ...
-        // Ok, I was wrong
-        for (int i=0; i<convexVerts.Length; i++) {
-            convexIndices[i] = i; //3*(i/3) + (i+1)%3 - 1;
+            indices[i] = i;
         }
         
         // Create the mesh
         Mesh msh = new Mesh();
         msh.Clear();
         msh.vertices = concaveVerts;
-        msh.triangles = concaveIndices;
+        msh.triangles = indices;
         msh.RecalculateNormals();
         
         // Set up game object with mesh
@@ -50,11 +39,9 @@ abstract public class Mesh_parent : MonoBehaviour {
         Mesh msh = gameObject.GetComponent<MeshFilter>().mesh;
         if (cavexFlag) {
             msh.vertices = convexVerts;
-            msh.triangles = convexIndices;
             cavexFlag = false;
         } else {
             msh.vertices = concaveVerts;
-            msh.triangles = concaveIndices;
             cavexFlag = true;
         }
         msh.RecalculateNormals();
