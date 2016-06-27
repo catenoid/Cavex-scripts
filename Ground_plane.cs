22f862563c4d6b1cb2ca4f82775a7446e60e523b
catenoid <hackphilippa@gmail.com>
2016-06-27 14:35:55 +0100

    Ground plane adjusted to conform to flared steps

--- Ground_plane.cs
+++ Ground_plane.cs
@@ -5,13 +5,14 @@ public class Ground_plane : Visible_surface {
     public Ground_plane () {
         Vector3[] stairVerts = new Vector3[] {
             new Vector3(0, 0, 0),
-            new Vector3(-2, 0, 0),
+            new Vector3(-3, 0, 0),
             new Vector3(-2, 1, 0),
             new Vector3(-2, 1, -1),
             new Vector3(-2, 2, -1),
             new Vector3(-2, 2, -2),
             new Vector3(0, 2, -2),
-            new Vector3(0,0,-2),
+            new Vector3(0, 1, -2),
+            new Vector3(0,0,-3),
         };
         
         // Projection vector used for culling the parts of the ground plane hidden by the stairs
@@ -35,18 +36,18 @@ public class Ground_plane : Visible_surface {
         
         // Populate concaveVerts with the required bounding points and cull points
         concaveVerts = new Vector3[] {
-            bounds[0], culled[0], bounds[1],
-            culled[0], culled[1], bounds[1],
-            culled[1], culled[2], bounds[1],
-            culled[2], bounds[2], bounds[1],
-            culled[2], culled[4], bounds[2],
-            culled[3], culled[4], culled[2],
+            bounds[0], culled[0], culled[1],
+            bounds[0], culled[1], bounds[1],
+            culled[1], culled[3], bounds[1],
+            culled[3], culled[4], bounds[1],
+            culled[4], bounds[2], bounds[1],
             culled[5], bounds[2], culled[4],
             culled[6], bounds[2], culled[5],
-            bounds[3], bounds[2], culled[6],
+            culled[6], bounds[3], bounds[2],
             bounds[3], culled[6], culled[7],
-            bounds[3], culled[7], bounds[0],
-            bounds[0], culled[7], culled[0],
+            bounds[3], culled[7], culled[8],
+            bounds[3], culled[8], bounds[0],
+            bounds[0], culled[8], culled[0],
         };
     }
 }
